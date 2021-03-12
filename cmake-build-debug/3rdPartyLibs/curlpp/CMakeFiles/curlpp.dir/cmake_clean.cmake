e9057be9767106a0b3b2a4043ebc4858338fd262
Danylo Sahaidak <danylo.sahaidak@gmail.com>
2021-03-12 02:45:13 +0200

    remove build folder

--- cmake-build-debug/3rdPartyLibs/curlpp/CMakeFiles/curlpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcurlpp.a"
-  "libcurlpp.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/curlpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/3rdPartyLibs/glfw/CMakeFiles/GLFW.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libGLFW.a"
-  "libGLFW.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GLFW.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
