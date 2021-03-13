63d591fb772d982d745f2a57532f52b982a495f8
Danylo Sahaidak <danylo.sahaidak@gmail.com>
2021-03-13 11:11:01 +0200

    change linking of libs

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 
 #include "Window.h"
-#include "TileLoader.h"
+//#include "TileLoader.h"
+//#include "libs/curlpp/include/curlpp/Exception.hpp"
 
 int main()
 {
     Window window;
 
-//    TileLoader loader("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/");
-//    loader.GetTile(1,1,0);
+//    try {
+//        TileLoader loader("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/");
+//        loader.GetTile(1, 1, 0);
+//    }
+//    catch( cURLpp::RuntimeError &e )
+//    {
+//        std::cout << e.what() << std::endl;
+//    }
+//    catch( cURLpp::LogicError &e )
+//    {
+//        std::cout << e.what() << std::endl;
+//    }
+
 
     window.Temp();
     window.Render();
